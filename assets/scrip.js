f271c61c302a822e04f1b4943290a9ae5d8dc463
Sebinhas <jsebascuervo05@gmail.com>
2023-12-27 23:33:18 -0500

    JS arregle el componente de characters con los arrays de items

--- assets/scrip.js
+++ assets/scrip.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     componentsChargers("characters");
+    getCharacter();
 });
 
 const componentsChargers = (component) => {
@@ -7,5 +8,54 @@ const componentsChargers = (component) => {
         url: "components/"+component+".php"
     }).done(function(res) {
         $("#main").html(res);
+    });
+}
+
+
+const getCharacter = () => {
+    const apiUrl = "https://dragonball-api.com/api/characters";
+    fetch(apiUrl)
+    .then(response => response.json()) 
+    .then(data => processCharacter(data.items)) 
+    .catch(error => console.error("Error en la solicitud:", error));
+}
+
+const processCharacter = (characters) => {
+    console.log(characters);
+    $("#charactersCards").html("");
+    characters.forEach(character => {
+        $("#charactersCards").append(`<div class="card">
+        <div class="back">
+            <img src="${character.image}" alt="">
+        </div>
+
+        <div class="description">
+
+            <div class="name">
+                <h1>${character.name}</h1>
+            </div>
+
+            <div class="subtitulo">
+                <h3>${character.race} - ${character.gender}</h3>
+            </div>
+
+            <div class="titulo">
+                <h3>Total KI:</h3>
+            </div>
+
+            <div class="subtitulo">
+                <h3>${character.maxKi}</h3>
+            </div>
+            
+            <div class="titulo">
+                <h3>Group:</h3>
+            </div>
+
+            <div class="subtitulo">
+                <h3>${character.affiliation}</h3>
+            </div>
+            
+        </div>
+    </div>`);
     });
 }
--- components/characters.php
+++ components/characters.php
@@ -3,16 +3,8 @@
         arriba
     </div>
 
-    <div class="down">
-        <div class="card">
-            <div class="back">
-                <img src="https://res.cloudinary.com/dgtgbyo76/image/upload/v1699044374/hlpy6q013uw3itl5jzic.webp" alt="">
-            </div>
-
-            <div class="description">
-                
-            </div>
-        </div>
+    <div class="down" id="charactersCards">
+        
     </div>
 
 
