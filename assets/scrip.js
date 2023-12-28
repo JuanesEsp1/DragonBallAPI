874a2c9667a982a0ea91761e0c1666fc0cedc095
Sebinhas <jsebascuervo05@gmail.com>
2023-12-27 22:35:24 -0500

    JS agregue el componente characters

--- assets/scrip.js
+++ assets/scrip.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    componentsChargers("init");
+    componentsChargers("characters");
 });
 
 const componentsChargers = (component) => {
--- components/characters.php
+++ components/characters.php
@@ -1,3 +1,19 @@
-<section>
-    hola soy el componente de characters
+<section id="characters">
+    <div class="up">
+        arriba
+    </div>
+
+    <div class="down">
+        <div class="card">
+            <div class="back">
+                <img src="https://res.cloudinary.com/dgtgbyo76/image/upload/v1699044374/hlpy6q013uw3itl5jzic.webp" alt="">
+            </div>
+
+            <div class="description">
+                
+            </div>
+        </div>
+    </div>
+
+
 </section>
